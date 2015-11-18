0963c25bc4713db30f3903f801d30263b078a825
bjuncklaus <bjuncklaus@gmail.com>
2015-11-18 12:33:49 -0500

    Did some code organization and documentation. Added some TODO's. Refactored some variables to keep a standard.

--- catFun.py
+++ catFun.py
@@ -5,28 +5,10 @@ from random import randint
 
 ################################################################
 
-# This program is an interactive simulation/game. A cat starts
-# to move across the screen. The direction of movement is reversed
-# on each "mouse down" event.
-#
-# The state of the cat is represented by a tuple (pos, delta-pos).
-# The first element, pos, represents the x-coordinate of the cat.
-# The second element, delta-pos, represents the amount that the
-# position changes on each iteration of the simulation loop.
-#
-# For example, the tuple (7,1) would represent the cat at x-coord,
-# 7, and moving to the right by 1 pixel per "clock tick."
-#
-# The initial state of the cat in this program is (0,1), meaning that the cat
-# starts at the left of the screen and moves right one pixel per tick.
-#
-# Pressing a mouse button down while this simulation run updates the cat state
-# by leaving pos unchanged but reversing delta-pos (changing 1 to -1 and vice
-# versa). That is, pressing a mouse key reverses the direction of the
-# cat.
-#
-# The simulation ends when the cat is allowed to reach either the left
-# or the right edge of the screen.
+# DESCRIPTION
+# OF
+# THE
+# GAME
 
 ################################################################
 
@@ -36,28 +18,29 @@ score = 0
 scoreText = ""
 
 # Static variables
-droneSize = 48
-deliverySize = 16
+DRONE_SIZE = 48
+DELIVERY_SIZE = 16
+WIDTH = 1024
+HEIGHT = 700
 
 # Initialize world
+# TODO - instead of creating a new window for the scores, maybe just erase everything and change the title
 name = "Drolivery - Fast & Accurate Drone Delivery"
-width = 1024
-height = 700
-rw.newDisplay(width, height, name)
 
-# initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
+rw.newDisplay(WIDTH, HEIGHT, name)
+
+# Initialize font -> must be called after pygame.init() to avoid 'Font not Initialized' error
 font = pg.font.Font(None, 35)
 
 ################################################################
 
-# Display the state by drawing a cat at that x coordinate
+# Loads the images
 droneImage = dw.loadImage("drone.png")
 deliveryImage = dw.loadImage("delivery.png")
 
-# state -> image (IO)
-# draw the cat halfway up the screen (height/2) and at the x
-# coordinate given by the first component of the state tuple
-#
+"""
+DOCUMENT
+"""
 def updateDisplay(state):
     dw.fill(dw.white)
     dw.draw(droneImage, (state[0], state[2]))
@@ -68,16 +51,13 @@ def updateDisplay(state):
     scoreText = font.render("Score: " + str(score), 1, (0, 0, 0))
 
 
-    dw.draw(scoreText, (width / 2, 5))
+    dw.draw(scoreText, (WIDTH / 2, 5))
 
 ################################################################
 
-# Change pos by delta-pos, leaving delta-pos unchanged
-# Note that pos is accessed as state[0], and delta-pos
-# as state[1]. Later on we'll see how to access state
-# components by name (as we saw with records in Idris).
-#
-# state -> state
+"""
+DOCUMENT
+"""
 def updateState(state):
     global score
     score += 1
@@ -85,17 +65,24 @@ def updateState(state):
 
 ################################################################
 
-# Terminate the simulation when the x coord reaches the screen edge,
-# that is, when pos is less then zero or greater than the screen width
-# state -> bool
+"""
+DOCUMENT
+"""
 def endState(state):
+    # TODO - if end(state) returns true, change the screen to the score record screen
+    if (end(state)):
+        # TODO - create a boolean variable to control the state of the game
+        None
     return (end(state))
 
 
 def end(state):
-    return touching(state) or state[0] > (width - droneSize) or state[2] > (height - droneSize) or state[0] < 0 or state[2] < 0
+    return touching(state) or state[0] > (WIDTH - DRONE_SIZE) or state[2] > (HEIGHT - DRONE_SIZE) or state[0] < 0 or state[2] < 0
 
 
+"""
+Checks whether or not any part of the drone touched any part of the delivery box
+"""
 def touching(state):
     for i in droneBoundaries(state[0]):
         if (i in touchRange[0]):
@@ -105,33 +92,33 @@ def touching(state):
 
     return False
 
+"""
+Retrieves all the pixels relative to the area that the drone covers
+"""
 def droneBoundaries(axis):
     b = []
-    for i in range(axis, axis + droneSize):
+    for i in range(axis, axis + DRONE_SIZE):
         b.append(i)
 
     return b
 
 ################################################################
 
-# We handle each event by printing (a serialized version of) it on the console
-# and by then responding to the event. If the event is not a "mouse button down
-# event" we ignore it by just returning the current state unchanged. Otherwise
-# we return a new state, with pos the same as in the original state, but
-# delta-pos reversed: if the cat was moving right, we update delta-pos so that
-# it moves left, and vice versa. Each mouse down event changes the cat
-# direction. The game is to keep the cat alive by not letting it run off the
-# edge of the screen.
-#
-# state -> event -> state
-#
+"""
+DOCUMENT
+"""
 def handleEvent(state, event):
+    # TODO - catch the event handler to get the spacebar or some other key and set the state of the game var to true, the user can only change it when the previous game is ended
     if (event.type == pg.MOUSEBUTTONDOWN):
         newState = [randomSpeed(), randomSpeed()]
         return((state[0], newState[0], state[2], newState[1]))
     else:
         return(state)
 
+
+"""
+Generates a random speed value different from 0
+"""
 def randomSpeed():
     r = randint(-4,4)
     if (r == 0):
@@ -139,21 +126,31 @@ def randomSpeed():
 
     return r
 
+"""
+Generates a random number between the given boundaries
+"""
 def randomAxys(min, max):
     return randint(min, max)
 ################################################################
 
-# The cat starts at the left, moving right
+# The drone starts in a random position between the top and halfway of the screen
 #initState = (80, 1, 80, 1)
-initState = (0, 3, randomAxys(0, (height-350)), 2)
+initState = (0, 3, randomAxys(0, (HEIGHT - 350)), 2)
+# The delivery box starts in a random position near the end of the screen to the right side
 #deliveryInitState = (100, 100)
 deliveryInitState = (randomAxys(984, 1010), randomAxys(17, 682))
 
-for i in range(deliveryInitState[0], deliveryInitState[0] + deliverySize):
-    touchRange[0].append(i)
+"""
+Fills the touchRange list with the delivery box boundaries
+"""
+def getDeliveryBoundaries():
+    for i in range(deliveryInitState[0], deliveryInitState[0] + DELIVERY_SIZE):
+        touchRange[0].append(i)
+    for i in range(deliveryInitState[1], deliveryInitState[1] + DELIVERY_SIZE):
+        touchRange[1].append(i)
 
-for i in range(deliveryInitState[1], deliveryInitState[1] + deliverySize):
-    touchRange[1].append(i)
+# Filling the touchRange list
+getDeliveryBoundaries()
 
 print(touchRange)
 # Run the simulation no faster than 60 frames per second
